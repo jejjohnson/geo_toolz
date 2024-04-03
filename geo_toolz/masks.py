a0bb7aa87bfeab3e93421bfd305b165396879fdd
jejjohnson <emanjohnson91@gmail.com>
2024-04-03 02:43:22 +0200

    Updated ocean masks.

---
+++ geo_toolz/masks.py
@@ -0,0 +1,10 @@
+from geo_toolz._src.masks.ocean import add_ocean_mask
+from geo_toolz._src.masks.country import add_country_mask
+from geo_toolz._src.masks.land import add_land_mask
+
+
+__all__ = [
+    "add_ocean_mask", 
+    "add_country_mask", 
+    "add_land_mask", 
+]
